b3a6a5f6dcb387ad9c7d1bd4dd89dc36e934f883
themousepotato <navaneeths1998@gmail.com>
2018-10-05 00:33:17 +0530

    update

--- 4/week4/Autonomous+driving+application+-+Car+detection+-+v3.py
+++ 4/week4/Autonomous+driving+application+-+Car+detection+-+v3.py
@@ -293,23 +293,27 @@ def iou(box1, box2):
 
     # Calculate the (y1, x1, y2, x2) coordinates of the intersection of box1 and box2. Calculate its Area.
     ### START CODE HERE ### (≈ 5 lines)
-    xi1 = max(box1[0], box2[0])
-    yi1 = max(box1[1], box2[1])
-    xi2 = min(box1[2], box2[2])
-    yi2 = min(box1[3], box2[3])
-    inter_area = (xi2 - xi1)*(yi2 - yi1)
+    #xi1 = maximum of the x1 coordinates of the two boxes. 
+    xi1 = np.maximum(box1[0], box2[0])
+    #yi1 = maximum of the y1 coordinates of the two boxes
+    yi1 = np.maximum(box1[1], box2[1])
+    #xi2 = minimum of the x2 coordinates of the two boxes
+    xi2 = np.minimum(box1[2], box2[2])
+    #yi2 = minimum of the y2 coordinates of the two boxes
+    yi2 = np.minimum(box1[3], box2[3])
+    inter_area = (xi2-xi1)*(yi2-yi1)
     ### END CODE HERE ###    
 
     # Calculate the Union area by using Formula: Union(A,B) = A + B - Inter(A,B)
     ### START CODE HERE ### (≈ 3 lines)
-    box1_area = (box1[3] - box1[1])*(box1[2]- box1[0])
-    box2_area = (box2[3] - box2[1])*(box2[2]- box2[0])
-    union_area = (box1_area + box2_area) - inter_area
+    box1_area = (box1[2]-box1[0])*(box1[3]-box1[1])
+    box2_area = (box2[2]-box2[0])*(box2[3]-box2[1])
+    union_area = box1_area+box2_area-inter_area
     ### END CODE HERE ###
     
     # compute the IoU
     ### START CODE HERE ### (≈ 1 line)
-    iou = inter_area / union_area
+    iou = inter_area/union_area
     ### END CODE HERE ###
 
     return iou
